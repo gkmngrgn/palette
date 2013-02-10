8f244e886c126d4bb35958c7cd17ca1ef802e834
Gokmen Gorgen <gokmen@alageek.com>
2013-02-10 23:33:24 +0200

    show flash notification (ehe.)

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -5,9 +5,26 @@ class BaseHandler(tornado.web.RequestHandler):
     """
     It has some custom method for the project.
     """
-    def get_current_user(self):
+    template_name = None
+
+    def __init__(self, *args, **kwargs):
+        super(BaseHandler, self).__init__(*args, **kwargs)
+
+        # for translatable strings
+        self._ = self.locale.translate
+
+    def add_message(self, msg, msg_type='info'):
         """
-        Returns current user for the session.
+        Adds flash notification message to context.
         """
-        # TODO: please fill this method after integrated mongodb database.
-        return
+        return {'content': msg, 'type': msg_type}
+
+    def get(self):
+        context = self.get_context_data()
+        return self.render(self.template_name, **context)
+
+    def get_context_data(self):
+        return {'messages': []}
+
+    def post(self):
+        return super(BaseHandler, self).get()
--- handlers/home.py
+++ handlers/home.py
@@ -5,5 +5,15 @@ class HomeHandler(BaseHandler):
     """
     Home page.
     """
-    def get(self):
-        return self.render('home.html')
+    template_name = 'home.html'
+
+    def post(self):
+        context = self.get_context_data()
+        image = self.request.files.get('image', [None])[0]
+
+        if image is None:
+            context['messages'].append(
+                self.add_message(
+                    self._("Image field is required."), 'error'))
+
+        return self.render(self.template_name, **context)
